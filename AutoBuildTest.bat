2bd6de29f6be2f36334a247214b27e252a7a62ad
marut.prasad <Marut.Prasad@philips.com>
2019-07-16 14:53:07 +0530

    Milestone 2

---
+++ AutoBuildTest.bat
@@ -0,0 +1,5 @@
+@echo OFF
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe RulesBasedAlertingSystem\RulesBasedAlertingSystem.vcxproj
+echo "build completed"
+RulesBasedAlertingSystem\Debug\RulesBasedAlertingSystem.exe
+pause
---
+++ README.md
@@ -0,0 +1,1 @@
+# Rules Based Alerting System
