09636dfde3d72a298be3ec2ce452f60b6bec1e4b
marut.prasad <Marut.Prasad@philips.com>
2019-07-29 16:15:53 +0530

    Code Base

--- AutoBuildTest.bat
+++ AutoBuildTest.bat
@@ -1,5 +1,7 @@
 @echo OFF
 devenv /clean debug RulesBasedAlertingSystem\RulesBasedAlertingSystem.sln
 devenv /build debug RulesBasedAlertingSystem\RulesBasedAlertingSystem.sln
-RulesBasedAlertingSystem\Debug\RulesBasedAlertingSystem.Host.exe
+   set /p answer=Open in Registration Mode (Y/N)?
+   if /i "%answer:~,1%" EQU "Y" RulesBasedAlertingSystem\Debug\RulesBasedAlertingSystem.Host.exe
+   if /i "%answer:~,1%" EQU "N" exit /b
 pause
